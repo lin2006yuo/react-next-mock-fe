ee3ef2df75b568a46efb4a3b51d94aa82bfe4545
linxueyu <lin2006yuo@gmail.com>
2020-08-17 22:36:53 +0800

    3000

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
 export const domain =
-  process.env.NODE_ENV === "production" ? "http://119.29.101.122/api" : "http://localhost:3000/api"
+  process.env.NODE_ENV === "production" ? "http://119.29.101.122:3000/api" : "http://localhost:3000/api"
